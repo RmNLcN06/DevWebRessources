1fb321744e71f9f5b8ec180ce7bd6f6d230a52b0
RmNLcN06 <romain.luciano@orange.fr>
2022-08-22 13:45:39 +0200

    First Page Structure

---
+++ inc/_head.php
@@ -0,0 +1,35 @@
+<!-- Balises META -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" lang="fr" content="Romain Luciano" />
+<meta name="description" content="Bienvenue Sur DevWebRessources ! Le site de ressources et d'explications pour les dev web junior / senior !" />
+<meta name="keywords" lang="fr" content="DevWebRessources, DWR, HTML, CSS, Javascript, Typescript, PHP, Bootstrap, SASS" />
+<meta name="reply-to" content="https://github.com/RmNLcN06" />
+<meta name="copyright" content="COPYRIGHT Romain Luciano - Tous Droits Réservés" />
+<meta name="generator" lang="fr" content="Visual Studio Code, HTML5, CSS3, Javascript, PHP, SASS, Typescript" />
+<meta name="content-type" content ="text/html ;charset=ISO-8859-1" />
+<meta name="robots" content ="index, nofollow" />
+<!-- Balises META -->
+
+<!-- Title -->
+<title>Bienvenue sur DWR - Ressources et explications de développement web pour dev web junior et senior</title>
+<!-- Title -->
+
+<!-- Main Stylesheet CSS -->
+<link rel="stylesheet" href="dist/style.css">
+<!-- Main Stylesheet CSS -->
+
+<!-- Favicon Icon -->
+<link rel="shortcut icon" href="media/img/favicon.ico" type="image/x-icon">
+<!-- Favicon Icon -->
+
+<!-- Google Fonts Link -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+<!-- Google Fonts Link -->
+
+<!-- Font-Awesome CDN -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Font-Awesome CDN -->
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("inc/_head.php"); ?>
+</head>
+<body>
+    <div class="page">
+        <div class="wrapper">
+            <header>
+                <?php include("inc/_header.php"); ?>
+            </header>
+            <nav>
+                <?php include("inc/_nav.php"); ?>
+            </nav>
+            <main>
+                <?php include($pagepath); ?>
+            </main>
+            <footer>
+                <?php include("inc/_footer.php"); ?>
+            </footer>
+        </div>
+    </div>
+
+    <!-- Javascript Files -->
+    <script src="app/js/script.js"></script>
+</body>
+</html>
