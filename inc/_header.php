c6708fd7b0ea94782bee17a7bc18f8d3758ea921
RmNLcN06 <romain.luciano@orange.fr>
2022-09-20 16:46:25 +0200

    Add gap to grid structure

--- inc/_header.php
+++ inc/_header.php
@@ -1,1 +1,21 @@
-<p>Ceci est le header</p>
+<div class="header__logo">
+    <a href="?page=accueil">
+        <img src="" alt="">
+    </a>
+</div>
+<div class="header__user">
+    <ul>
+        <li>
+            <a href="">Inscription</a>
+        </li>
+        <li>
+            <a href="">Connexion</a>
+        </li>
+        <li>
+            <a href="">Déconnexion</a>
+        </li>
+        <li>
+            <a href="">Mon Profil</a>
+        </li>
+    </ul>
+</div>
