be3e41c297fa022f97f3bd5389825454c26e4f8f
RmNLcN06 <romain.luciano@orange.fr>
2022-09-21 16:29:01 +0200

    MQ on grid template in home page

--- inc/_nav.php
+++ inc/_nav.php
@@ -1,23 +1,20 @@
 <ul>
-    <h3>Le commencement</h3>
+    <li><a href="?page=start">Par où commencer</a></li>
+
     <li><a href="?page=algorithmie">Algorithmie</a></li>
 
-    <h3>Les bases</h3>
     <li><a href="?page=html">HTML</a></li>
     <li><a href="?page=css">CSS</a></li>
 
-    <h3>Front-End</h3>
     <li><a href="?page=javascript">Javascript</a></li>
-    <li><a href="?page=git">Git</a></li>
-
-    <h3>Back-End</h3>
-    <li><a href="?page=php">PHP</a></li>
-    <li><a href="?page=poo">Programmation Orientée Objet</a></li>
-    <li><a href="?page=sql">SQL</a></li>
-    <li><a href="?page=webpack">Webpack</a></li>
     <li><a href="?page=react">React</a></li>
     <li><a href="?page=vuejs">VueJS</a></li>
     <li><a href="?page=angular">Angular</a></li>
+
+    <li><a href="?page=git">Git</a></li>
+    <li><a href="?page=php">PHP</a></li>
+    <li><a href="?page=poo">Programmation Orientée Objet</a></li>
+    <li><a href="?page=sql">SQL</a></li>
     <li><a href="?page=symfony">Symfony</a></li>
     <li><a href="?page=laravel">Laravel</a></li>
 </ul>
